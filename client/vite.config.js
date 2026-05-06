3c0131477418493b134fd1eeb51cd7bbafbf7310
TheUIinventor <synchron.work@gmail.com>
2026-05-06 10:38:57 +1000

    enabling pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SBHS Timetable',
+        short_name: 'Timetable',
+        description: 'Sydney Boys High School Timetable App',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png',
+            sizes: '256x256',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
